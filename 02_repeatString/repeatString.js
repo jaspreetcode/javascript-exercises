2b63cef2ae8abffdc2f4d7916cab1eea7d539f9e
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2024-11-29 08:50:34 +0530

    Completed 02_repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    let alias = s;
+    if (n == -1) return "ERROR";
+    if (n == 0) return "";
+    for (i = 1; i < n; i++) {
+        s += alias;
+    }
+    return s;
 };
 
 // Do not edit below this line
