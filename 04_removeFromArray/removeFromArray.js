46e741d5e6251bb4d7aaf8b6e52ac06700039442
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2024-12-11 19:20:03 +0530

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let len = arr.length;
+    args.forEach((arg) => {
+        let i = 0;
+        while (i < len) {
+            if (arg === arr[i]) {
+                arr.splice(i, 1);
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+    });
+    return arr;
 };
 
 // Do not edit below this line
