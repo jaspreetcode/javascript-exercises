842bbaec77823acb26e14baf0afc89aacc823fd8
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2024-12-14 07:02:39 +0530

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const arr = string.toLowerCase().split('');
+    const newArr = arr.filter(element => (element != " " && element != "," && element != "." && element != "!"))
+    const newString = newArr.join('');
+    const reversedString = newArr.reverse().join('');
+    if (reversedString === newString) return true;
+    else return false;
 };
 
 // Do not edit below this line
