3e05ba1d40473a3211cb7721e0cdfaa4ebfb63c9
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2024-12-14 08:27:29 +0530

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const person = people.reduce((oldest, person) => {
+        const d = new Date();
+        const yearOfDeath = ("yearOfDeath" in person) ? person.yearOfDeath : d.getFullYear();
+        const age = yearOfDeath - person.yearOfBirth;
+        if (age > oldest.age) {
+            oldest = {...person, age};
+        }
+        return oldest
+    }, {age: 0});
+    delete person.age;
+    return person;
 };
 
 // Do not edit below this line
